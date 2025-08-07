c03bb0a5a20c876fa9b3cce12daa1d748a56a818
Erik Rodríguez Bernal <104200615+Tempeny@users.noreply.github.com>
2025-08-07 13:48:27 +0200

    Add User model

---
+++ dishNow/src/main/java/com/dishNow/dishNow/Models/User.java
@@ -0,0 +1,96 @@
+package com.dishNow.dishNow.Models;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String lastName;
+    private LocalDate birthday;
+    private String email;
+    private String passwordHash;
+    private String role = "user";
+    private boolean verified = false;
+
+    public User() {}
+
+    public User(String name, String lastName, LocalDate birthday, String email, String passwordHash) {
+        this.name = name;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = "user";
+        this.verified = false;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+}
