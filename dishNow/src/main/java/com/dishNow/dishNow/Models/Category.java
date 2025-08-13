598ed2b43762f8a5061da77b14a3921083f9fb5f
Erik Rodríguez Bernal <104200615+Tempeny@users.noreply.github.com>
2025-08-13 09:40:05 +0200

    Category Class Update

--- dishNow/src/main/java/com/dishNow/dishNow/Models/Category.java
+++ dishNow/src/main/java/com/dishNow/dishNow/Models/Category.java
@@ -2,11 +2,15 @@ package com.dishNow.dishNow.Models;
 
 public class Category {
     private int id;
-    private String name;
+    private String nameEN;
+    private String nameES;
+    private String nameCA;
 
-    public Category(int id, String name) {
+    public Category(int id, String nameEN, String nameES, String nameCA) {
         this.id = id;
-        this.name = name;
+        this.nameEN = nameEN;
+        this.nameES = nameES;
+        this.nameCA = nameCA;
     }
 
     // Getters
@@ -14,8 +18,16 @@ public class Category {
         return id;
     }
 
-    public String getName() {
-        return name;
+    public String getNameEN() {
+        return nameEN;
+    }
+
+    public String getNameES() {
+        return nameES;
+    }
+
+    public String getNameCA() {
+        return nameCA;
     }
 
     // Setters
@@ -23,8 +35,15 @@ public class Category {
         this.id = id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setNameEN(String nameEN) {
+        this.nameEN = nameEN;
     }
 
+    public void setNameES(String nameES) {
+        this.nameES = nameES;
+    }
+
+    public void setNameCA(String nameCA) {
+        this.nameCA = nameCA;
+    }
 }
