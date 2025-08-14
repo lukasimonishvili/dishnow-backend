f8bd8d99a71fb3f20a98a505ad2004a41abb6611
Tempeny <104200615+Tempeny@users.noreply.github.com>
2025-08-14 10:51:47 +0200

    Review: Correction on RegisterUserDto

--- dishNow/src/main/java/com/dishNow/dishNow/Models/RegisterUserDto.java
+++ dishNow/src/main/java/com/dishNow/dishNow/Models/RegisterUserDto.java
@@ -10,53 +10,21 @@ public class RegisterUserDto {
     private String password;
     private String confirmPassword;
 
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getName(){
-        return name;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public String setName(String name) {
-        return this.name = name;
-    }
+    public LocalDate getBirthday() { return birthday; }
+    public void setBirthday(LocalDate birthday) { this.birthday = birthday; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public LocalDate getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(LocalDate birthday) {
-        this.birthday = birthday;
-    }
-
-    public String getEmail(){
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
+    public String getConfirmPassword() { return confirmPassword; }
+    public void setConfirmPassword(String confirmPassword) { this.confirmPassword = confirmPassword; }
 }
