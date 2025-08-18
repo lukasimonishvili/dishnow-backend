eddf71888a2d434849bc0f390181c59edac5429a
JinDaifer <thedaifer1@gmail.com>
2025-08-18 14:32:37 +0200

    Update SwaggerConfig.java

--- dishNow/src/main/java/com/dishNow/dishNow/Config/SwaggerConfig.java
+++ dishNow/src/main/java/com/dishNow/dishNow/Config/SwaggerConfig.java
@@ -1,12 +1,13 @@
 package com.dishNow.dishNow.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.parameters.Parameter;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.parameters.ParameterIn;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
